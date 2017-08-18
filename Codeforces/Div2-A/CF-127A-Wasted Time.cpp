cd5817bfb3c011401c1234ecf15115a5def2fdc3
Mahmoud Tarek <ma7moud.tarek97@gmail.com>
2017-08-18 14:59:32 +0200

    Create CF-127A-Wasted Time.cpp

---
+++ Codeforces/Div2-A/CF-127A-Wasted Time.cpp
@@ -0,0 +1,36 @@
+/***********************************************/
+/**                                           **/
+/**   CF-D2-127A-Wasted Time.cpp              **/
+/**   Credits to Mahmoud Tarek Samir          **/
+/**   Ma7moud.tarek97@gmail.com               **/
+/**                                           **/
+/***********************************************/
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+
+double length(double x1,double y1,double x2,double y2)
+{
+    return sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) ) ;
+}
+
+int main()
+{
+    int n,k;
+    cin >> n >> k;
+    int x[n];
+    int y[n];
+    for(int i=0; i<n; i++)
+    {
+        cin >> x[i] >> y[i];
+    }
+    double sign_length=0;
+    for(int i=1; i<n; i++)
+    {
+        sign_length+=length(x[i-1],y[i-1],x[i],y[i]);
+    }
+    printf("%lf\n",sign_length*k/50.0);
+    return 0;
+}
